123ac7f8e2b0d5721f26f6297d1c69f2e89ca1ea
JattinTHAKUR <104769463+JattinTHAKUR@users.noreply.github.com>
2023-08-24 06:49:36 +0000

    commit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+
+void merge(int arr1[], int n, int arr2[], int m, int arr3[]) {
+  int i = 0; //first index of arr1
+  int j = 0; //first index of arr2
+  while(i<n && j<m){
+    
+  }
+}
+
 
 int main() {
-  std::cout << "Hello World!\n";
+
+
+  int arr1[5] = {1,3,5,7,9};
+  int arr2[4] = {2,4,6,8};
+  
+  int arr3[9] = {0};
+
+  merge(arr1, 5, arr2, 4, arr3 );
+  print(arr3,9);
+
+  return 0;
+  
 }
