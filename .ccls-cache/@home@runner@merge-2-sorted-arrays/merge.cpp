c5a7caf3651f763d8c9dfd152af5e2695873f697
JattinTHAKUR <104769463+JattinTHAKUR@users.noreply.github.com>
2023-08-24 09:13:52 +0000

    commit

--- .ccls-cache/@home@runner@merge-2-sorted-arrays/merge.cpp
+++ .ccls-cache/@home@runner@merge-2-sorted-arrays/merge.cpp
@@ -5,13 +5,40 @@ using namespace std;
 void merge(int arr1[], int n, int arr2[], int m, int arr3[]) {
   int i = 0; //first index of arr1
   int j = 0; //first index of arr2
+  int k = 0;
   while(i<n && j<m){
     if(arr1[i] < arr2[j]){
-      arr3[]
+      arr3[k] = arr1[i];
+      k++;
+      i++;
+    }
+    else{
+      arr3[k] = arr2[j]; //second wale arr ki value choti h
+      k++;
+      j++;
     }
   }
+  //mtlb first arr ke elemnt khtm ho gye hai isliye 
+  //copy elemenyts of first arr 
+  while(i<n){
+    arr3[k] = arr1[i];
+    k++;
+    i++;
+  }
+  //mtlb second arr ke elemnt khtm ho gye hai isliye 
+  //copy lements of second array 
+  while(j<m){
+    arr3[k] = arr2[j];
+    k++;
+    j++;
+  }
 }
-
+void print(int ans[], int n){
+  for(int i=0; i<n; i++){
+    cout<< ans[i] <<" ";
+  }
+  cout<<endl;
+}
 
 int main() {
 
